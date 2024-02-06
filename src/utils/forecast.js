483d5609e901ce2d810ae46ee55b62807dae86b4
Baccin Michael <michael@MacBook-Air-di-Baccin.local>
2024-02-06 11:41:09 +0100

    modificato request app

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,4 +1,4 @@
-const request = require("postman-request");
+const request = require("request");
 const forecast = (latitude, longitude, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=fd8aeb0167ad6b2f1169c536ed35535c&query='+latitude+','+longitude
     request({url, json: true}, (error, {body}) => {
--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,4 +1,4 @@
-const request = require("postman-request");
+const request = require("request");
 const geocode = (address, callback) => {
     const url = 'http://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + '.json?access_token=pk.eyJ1IjoibWljaGFlbGJhY2NpbiIsImEiOiJjbGRvdGI5YmMwdnloM29uMGYxeXVnbG51In0.O3HDNj_hJauuQsWm4ONFhw'
     request({url,json: true}, (error, {body}) => {

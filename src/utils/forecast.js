c8e750ad17cc379111fa8300e8a5ffd62fbe9271
Baccin Michael <michael@MacBook-Air-di-Baccin.local>
2024-02-06 11:58:53 +0100

    aggiornato informazioni di ritorno app meteo

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -7,7 +7,8 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. Feels like ' + body.current.feelslike + ' degrees out.')
+
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. Feels like ' + body.current.feelslike + ' degrees out.' + ' the humidity is ' +  body.current.humidity + ' %')
 
         }
     })
